92f97b4ae5c1e9af5bf6bf8a3641c35a2576f9da
Volodymyr Antoniuk <voxtobox@gmail.com>
2021-03-19 15:46:21 +0200

    fix init validation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import ValidationLevel from './ValidationLevel';
 
-export default function (data: any) {
-    return new ValidationLevel(data);
+export default function (rules: object, model: object): ValidationLevel {
+    return new ValidationLevel(rules, model);
 }
 
 export { ValidationLevel };

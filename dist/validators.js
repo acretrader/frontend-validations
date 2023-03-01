b2a14737769b0863e4bb2ea9414c6c93b4a46202
nelyaklyusa <nelya.klyusa@gmail.com>
2023-03-01 15:01:20 +0200

    add date validator
    Signed-off-by: nelyaklyusa <nelya.klyusa@gmail.com>

--- dist/validators.js
+++ dist/validators.js
@@ -11,7 +11,7 @@ const getLength = (value) => {
     }
     return String(value).length;
 };
-const birthdayValidator = (value) => {
+const dateValidator = (value) => {
     if (!req(value))
         return true;
     if (!/^\d{4}-\d{2}-\d{2}$/.test(value))
@@ -115,18 +115,19 @@ export const regex = (param) => withParams({ type: 'regex', value: param }, (val
 export const password = (param) => withParams({ type: 'password', value: param }, (value) => !req(value) || /\S{8,}/.test(value));
 export const email = (param) => withParams({ type: 'email', value: param }, (value) => !req(value) || emailRegex.test(value));
 export const futureDate = (param) => withParams({ type: 'futureDate', value: param }, (value) => {
-    if (!req(value) || !birthdayValidator(value))
+    if (!req(value) || !dateValidator(value))
         return true;
     return futureDateValidator(value);
 });
 export const legalAge = (param) => withParams({ type: 'legalAge', value: param }, (value) => {
-    if (!req(value) || !birthdayValidator(value)
+    if (!req(value) || !dateValidator(value)
         || !futureDateValidator(value))
         return true;
     const maxDateParam = +(new Date(new Date().setFullYear(new Date().getFullYear() - 18)));
     return +(new Date(value)) <= maxDateParam;
 });
-export const birthday = (param) => withParams({ type: 'birthday', value: param }, birthdayValidator);
+export const birthday = (param) => withParams({ type: 'birthday', value: param }, dateValidator);
+export const date = (param) => withParams({ type: 'date', value: param }, dateValidator);
 export const minLength = (param) => withParams({ type: 'minLength', value: param }, (value) => (!req(value) || getLength(value) >= param));
 export const maxLength = (param) => withParams({ type: 'maxLength', value: param }, (value) => (!req(value) || getLength(value) <= param));
 export const minValue = (param) => withParams({ type: 'minValue', value: param }, (value) => (!req(value) || ((!/\s/.test(value) || value instanceof Date) && +value >= +param)));
